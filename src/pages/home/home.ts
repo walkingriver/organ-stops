95bca3d87213d712ea7aa96038355d2724761980
Ben Callaghan <benj.callaghan@gmail.com>
2017-08-07 14:35:11 -0400

    Added a basic home page that lists the hymns

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,6 +6,11 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  hymns = [
+    { number: 1, title: 'First Hymn' },
+    { number: 2, title: 'Second Hymn' },
+    { number: 3, title: 'Third Hymn' }
+  ];
 
   constructor(public navCtrl: NavController) {
 

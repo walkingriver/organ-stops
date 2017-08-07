8c9ae9ab7ec2c11a6e74d85d89c3a27bbba3fbb8
Ben Callaghan <benj.callaghan@gmail.com>
2017-08-07 16:27:12 -0400

    Added sample data to display (Hymn 1)

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,7 +9,55 @@ import { Hymn } from '../../app/hymn';
 })
 export class HomePage {
   hymns: Hymn[] = [
-    { number: 1, title: 'First Hymn', pedal: {}, swell: {}, great: {}, general: {} },
+    { number: 1, title: 'First Hymn', pedal: {
+      'Contra Violone 32': true,
+      'Diapason 16': false,
+      'Bourdon 16': false,
+      'Lieblich Gedackt 16': false,
+      'Octave 8': true,
+      'Flute 8': false,
+      'Choral Bass 4': false,
+      'Mixture III': true,
+      'Posaune 16': true,
+      'Waldhorn 16': false,
+      'French Trumpet 8': true,
+      'Great to Pedal': false,
+      'Swell to Pedal': false
+    }, swell: {
+      'Lieblich Gedackt 16': false,
+      'Gedackt 8': true,
+      'Viola Pomposa 8': true,
+      'Viola Celeste 8': false,
+      'Flute Celeste II 8': false,
+      'Octave 4': false,
+      'Traverse Flute 4': false,
+      'Nasard 2 2/3': true,
+      'Piccolo 2': true,
+      'Tierce 1 3/5': false,
+      'Fourniture IV': false,
+      'Waldhorn 16': true,
+      'Tromba 8': true,
+      'Oboe 8': false,
+      'Treumulant': false
+    }, great: {
+      'Violone 16': false,
+      'Diapason 8': true,
+      'Harmonic Flute 8': false,
+      'Flute Celeste II 8': false,
+      'Octave 4': false,
+      'Spitzflöte 4': true,
+      'Fifteenth 2': true,
+      'Mixture IV': false,
+      'French Trumpet 8': true,
+      'Krummhorn 8': false,
+      'Chimes': true,
+      'Tremulant': false,
+      'Swell to Great': false
+    }, general: {
+      'Bass Coupler': true,
+      'Melody Coupler': false,
+      'Alternate Tuning': false
+    } },
     { number: 2, title: 'Second Hymn', pedal: {}, swell: {}, great: {}, general: {} },
     { number: 3, title: 'Third Hymn', pedal: {}, swell: {}, great: {}, general: {} }
   ];
--- src/pages/stops/stops.ts
+++ src/pages/stops/stops.ts
@@ -1,24 +1,17 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the StopsPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
+import { OrganStops } from '../../app/organ-stops';
 
 @Component({
   selector: 'page-stops',
   templateUrl: 'stops.html',
 })
 export class StopsPage {
+  stops: OrganStops;
+  stopNames: string[];
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.stops = navParams.data;
+    this.stopNames = Object.keys(this.stops);
   }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad StopsPage');
-  }
-
 }

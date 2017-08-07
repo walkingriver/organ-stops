1d109820a63a6f7fb3f6b3909fea36e9855cf08d
Ben Callaghan <benj.callaghan@gmail.com>
2017-08-07 18:56:58 -0400

    Added sort buttons

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { BehaviorSubject } from 'rxjs/behaviorsubject';
+import { Observable } from 'rxjs/observable';
 
 import { HymnPage } from '../hymn/hymn';
 import { Hymn } from '../../app/hymn';
@@ -12,12 +14,22 @@ import { Hymn } from '../../app/hymn';
 export class HomePage {
   hymns: FirebaseListObservable<Hymn[]>;
 
+  sort = 'number';
+  sortSubject = new BehaviorSubject(this.sort);
+
   constructor(public navCtrl: NavController, private db: AngularFireDatabase) {
-    this.hymns = db.list('/hymns');
+    this.hymns = db.list('/hymns', {
+      query: {
+        orderByChild: this.sortSubject as Observable<string>
+      }
+    });
   }
 
   nav(hymn: Hymn) {
     this.navCtrl.push(HymnPage, hymn);
   }
 
+  sortBy(prop: string) {
+    this.sortSubject.next(prop);
+  }
 }

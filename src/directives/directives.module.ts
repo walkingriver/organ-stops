93395f7127f8ca9c206bdeae9b104dc21dc15db9
Ben Callaghan <benj.callaghan@gmail.com>
2017-08-14 20:29:17 -0400

    Removed the unnecessary svg directive

--- src/directives/directives.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { SvgReplaceDirective } from './svgreplace/svgreplace';
-
-@NgModule({
-    declarations: [SvgReplaceDirective],
-    imports: [],
-    exports: [SvgReplaceDirective]
-})
-export class DirectivesModule {}

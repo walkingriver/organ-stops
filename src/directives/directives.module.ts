ee350a4134e1a8ff6b722854b8271961b107c615
Ben Callaghan <benj.callaghan@gmail.com>
2017-08-14 20:18:13 -0400

    Added a new directive to handle SVG files

---
+++ src/directives/directives.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { SvgReplaceDirective } from './svgreplace/svgreplace';
+
+@NgModule({
+    declarations: [SvgReplaceDirective],
+    imports: [],
+    exports: [SvgReplaceDirective]
+})
+export class DirectivesModule {}

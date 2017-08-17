0a9088e73a89bba9b2c48baab5eea94409fa5039
Ben Callaghan <benj.callaghan@gmail.com>
2017-08-17 13:46:35 -0400

    Added a form for email/password authentication

--- src/pages/user/user.ts
+++ src/pages/user/user.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase/app';
 
@@ -8,11 +9,21 @@ import * as firebase from 'firebase/app';
 })
 export class UserPage {
   user: firebase.User;
+  form: FormGroup;
 
-  constructor(private afAuth: AngularFireAuth) {
+  constructor(private afAuth: AngularFireAuth, fb: FormBuilder) {
     afAuth.authState.subscribe(user => {
       this.user = user;
     });
+
+    this.form = fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  signInWithEmail() {
+    this.afAuth.auth.signInWithEmailAndPassword(this.form.value.email, this.form.value.password);
   }
 
   signInWithFacebook() {

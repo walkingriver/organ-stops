ecccbf48ad4535d5fb916f61eae1070e2d38cdef
Ben Callaghan <benj.callaghan@gmail.com>
2020-11-07 23:18:28 -0700

    Remove unused default component

--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -1,19 +1,11 @@
 import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { AboutPage } from './about.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 import { AboutPageRoutingModule } from './about-routing.module';
 
 @NgModule({
-  imports: [
-    IonicModule,
-    CommonModule,
-    FormsModule,
-    ExploreContainerComponentModule,
-    AboutPageRoutingModule,
-  ],
+  imports: [IonicModule, CommonModule, AboutPageRoutingModule],
   declarations: [AboutPage],
 })
 export class AboutPageModule {}

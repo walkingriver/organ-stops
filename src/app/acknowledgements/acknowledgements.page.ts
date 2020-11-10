8363d5def4d0cff6b537bec13260638836a7b005
Ben Callaghan <benj.callaghan@gmail.com>
2020-11-09 17:41:05 -0700

    Replace http call with default import

--- src/app/acknowledgements/acknowledgements.page.ts
+++ src/app/acknowledgements/acknowledgements.page.ts
@@ -1,22 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
 import { License } from '../license';
-import { groupBy, map, mergeMap, toArray } from 'rxjs/operators';
-import { Observable } from 'rxjs';
+import * as licenses from 'src/assets/licenses.json';
 
 @Component({
   selector: 'app-acknowledgements',
   templateUrl: './acknowledgements.page.html',
   styleUrls: ['./acknowledgements.page.scss'],
 })
-export class AcknowledgementsPage implements OnInit {
-  licenses$: Observable<License[]>;
-
-  constructor(private http: HttpClient) {
-    this.licenses$ = http
-      .get<{ [name: string]: License }>('assets/licenses.json')
-      .pipe(map((licenses) => Object.values(licenses)));
-  }
-
-  ngOnInit() {}
+export class AcknowledgementsPage {
+  licenses: { [name: string]: License } = (licenses as any).default;
 }
